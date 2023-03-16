8b9c30af5e295906c3ad746f24da43ef2e2837dd
XuMing <ming.xu@kushuai.com>
2023-03-16 22:56:31 +0800

    style: rrt

---
+++ rrt/rrt.cpp
@@ -0,0 +1,115 @@
+#include "rrt.h"
+
+Node* RRT::GetNearestNode(const std::vector<double>& random_position) {
+  int minID = -1;
+  double minDistance = std::numeric_limits<double>::max();
+
+  for (int i = 0; i < node_list_.size(); i++) {
+    double square_distance =
+        pow(node_list_[i]->point().x - random_position[0], 2) +
+        pow(node_list_[i]->point().y - random_position[1], 2);
+    if (square_distance < minDistance) {
+      minDistance = square_distance;
+      minID = i;
+    }
+  }
+
+  return node_list_[minID];
+}
+
+bool RRT::collisionCheck(Node* newNode) {
+  for (auto item : obstacle_list_)
+    if (sqrt(pow((item[0] - newNode->point().x), 2) +
+             std::pow((item[1] - newNode->point().y), 2)) <= item[2])
+      return false;
+  return true;
+}
+
+std::vector<Node*> RRT::planning() {
+  cv::namedWindow("RRT");
+
+  int count = 0;
+
+  constexpr int kImageSize = 15;
+  constexpr int kImageResolution = 50;
+  cv::Mat background(kImageSize * kImageResolution,
+                     kImageSize * kImageResolution, CV_8UC3,
+                     cv::Scalar(255, 255, 255));
+
+  circle(background,
+         cv::Point(start_node_->point().x * kImageResolution,
+                   start_node_->point().y * kImageResolution),
+         20, cv::Scalar(0, 0, 255), -1);
+  circle(background,
+         cv::Point(goal_node_->point().x * kImageResolution,
+                   goal_node_->point().y * kImageResolution),
+         20, cv::Scalar(255, 0, 0), -1);
+
+  for (auto item : obstacle_list_) {
+    circle(background,
+           cv::Point(item[0] * kImageResolution, item[1] * kImageResolution),
+           item[2] * kImageResolution, cv::Scalar(0, 0, 0), -1);
+  }
+
+  node_list_.push_back(start_node_);
+  while (1) {
+    std::vector<double> random_position;
+    if (goal_dis_(goal_gen_) > goal_sample_rate_) {
+      double randX = area_dis_(goal_gen_);
+      double randY = area_dis_(goal_gen_);
+      random_position.push_back(randX);
+      random_position.push_back(randY);
+    } else {
+      random_position.push_back(goal_node_->point().x);
+      random_position.push_back(goal_node_->point().y);
+    }
+
+    Node* nearestNode = GetNearestNode(random_position);
+    double theta = atan2(random_position[1] - nearestNode->point().y,
+                         random_position[0] - nearestNode->point().x);
+    Node* newNode = new Node(nearestNode->point().x + step_size_ * cos(theta),
+                             nearestNode->point().y + step_size_ * sin(theta));
+    newNode->set_parent(nearestNode);
+
+    if (!collisionCheck(newNode)) continue;
+    node_list_.push_back(newNode);
+
+    line(background,
+         cv::Point(static_cast<int>(newNode->point().x * kImageResolution),
+                   static_cast<int>(newNode->point().y * kImageResolution)),
+         cv::Point(static_cast<int>(nearestNode->point().x * kImageResolution),
+                   static_cast<int>(nearestNode->point().y * kImageResolution)),
+         cv::Scalar(0, 255, 0), 10);
+
+    count++;
+    imshow("RRT", background);
+    cv::waitKey(5);
+
+    if (sqrt(pow(newNode->point().x - goal_node_->point().x, 2) +
+             pow(newNode->point().y - goal_node_->point().y, 2)) <=
+        step_size_) {
+      std::cout << "The path has been found!" << std::endl;
+      break;
+    }
+  }
+
+  std::vector<Node*> path;
+  path.push_back(goal_node_);
+  Node* tmpNode = node_list_.back();
+  while (tmpNode->parent() != nullptr) {
+    line(background,
+         cv::Point(static_cast<int>(tmpNode->point().x * kImageResolution),
+                   static_cast<int>(tmpNode->point().y * kImageResolution)),
+         cv::Point(
+             static_cast<int>(tmpNode->parent()->point().x * kImageResolution),
+             static_cast<int>(tmpNode->parent()->point().y * kImageResolution)),
+         cv::Scalar(255, 0, 255), 10);
+    path.push_back(tmpNode);
+    tmpNode = tmpNode->parent();
+  }
+
+  imshow("RRT", background);
+  cv::waitKey(0);
+  path.push_back(start_node_);
+  return path;
+}
---
+++ rrt/rrt.h
@@ -0,0 +1,67 @@
+#ifndef RRT_H_
+#define RRT_H_
+
+#include <cmath>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <random>
+#include <vector>
+
+struct Point {
+  Point(double x, double y) : x(x), y(y) {}
+  double x = 0.0;
+  double y = 0.0;
+};
+
+class Node {
+ public:
+  Node(double x, double y) : point_(x, y), parent_(nullptr), cost_(0.0) {}
+  const Point& point() const { return point_; }
+  void set_parent(Node* parent) { parent_ = parent; }
+  Node* parent() { return parent_; }
+
+ private:
+  Point point_;
+  std::vector<double> path_x_;
+  std::vector<double> path_y_;
+  Node* parent_ = nullptr;
+  double cost_ = 0.0;
+};
+
+class RRT {
+ public:
+  RRT(Node* start_node, Node* goal_node,
+      const std::vector<std::vector<double>>& obstacle_list,
+      double step_size = 1.0, int goal_sample_rate = 5)
+      : start_node_(start_node),
+        goal_node_(goal_node),
+        obstacle_list_(obstacle_list),
+        step_size_(step_size),
+        goal_sample_rate_(goal_sample_rate),
+        goal_gen_(goal_rd_()),
+        goal_dis_(std::uniform_int_distribution<int>(0, 100)),
+        area_gen_(area_rd_()),
+        area_dis_(std::uniform_real_distribution<double>(0, 15)) {}
+  Node* GetNearestNode(const std::vector<double>& random_position);
+  bool collisionCheck(Node*);
+  std::vector<Node*> planning();
+
+ private:
+  Node* start_node_;
+  Node* goal_node_;
+  std::vector<std::vector<double>> obstacle_list_;
+  std::vector<Node*> node_list_;
+  double step_size_;
+
+  int goal_sample_rate_;
+
+  std::random_device goal_rd_;
+  std::mt19937 goal_gen_;
+  std::uniform_int_distribution<int> goal_dis_;
+
+  std::random_device area_rd_;
+  std::mt19937 area_gen_;
+  std::uniform_real_distribution<double> area_dis_;
+};
+
+#endif
